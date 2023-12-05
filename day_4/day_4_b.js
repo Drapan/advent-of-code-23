ce410fc20f95ff36b512c29ea9875cdcebb3f30a
Mathias Andersson <drapans@gmail.com>
2023-12-05 19:38:28 +0000

    Day 4

---
+++ day_4/day_4_b.js
@@ -0,0 +1,40 @@
+import { readFileSync } from 'fs';
+
+const inputFilePath = new URL('./input.txt', import.meta.url)
+const input = readFileSync(inputFilePath, 'utf8');
+
+const lines = input.split('\n');
+
+const REGEX_PLAYED_NUMBERS = /(?<![|].*)\d+(?!\d*:)/gm;
+const REGEX_WINNING_NUMBERS = /(?<=[|]\s+.*)\d+/gm;
+
+const cards = new Array(lines.length).fill(1);
+
+for (let i = 0; i < lines.length; i++) {
+  const playedNumbers = lines[i].match(REGEX_PLAYED_NUMBERS).map((number) => parseInt(number, 10));
+  const winningNumbers = lines[i].match(REGEX_WINNING_NUMBERS).map((number) => parseInt(number, 10));
+
+  let matchedNumbers = 0;
+  for (const playedNumber of playedNumbers) {
+    if (winningNumbers.includes(playedNumber)) {
+      matchedNumbers++;
+    }
+  }
+
+  if (matchedNumbers > 0) {
+    const baseCount = cards[i];
+
+    for (let j = 0; j < matchedNumbers; j++) {
+      const destIndex = (i + 1) + j;
+
+      cards[destIndex] += baseCount;
+    }
+  }
+}
+
+let accumulator = 0;
+for (const count of cards) {
+  accumulator += count;
+}
+
+console.log(accumulator);
